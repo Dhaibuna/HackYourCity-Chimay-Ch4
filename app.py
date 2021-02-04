5fa73d0e63873620d33e35f753a4b08cf0262e87
Nooreyni <ousmane.diop1020@gmail.com>
2021-02-04 20:08:11 +0100

    before push caro

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask, request, render_template
+# Store this code in 'app.py' file 
+
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
-@app.route('/')
-def my_form():
-    return render_template('index.html')
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    variable = request.form['variable']
-    return variable
+if __name__ == '__main__':
+    app.run(port=6000)
 
 
-# <form method="POST">
-#     <input type= "text" name="Email" placeholder="Entrer votre mail"> 
-#     <input type= "text" name="Mot de passe" placeholder="Mot de passe"> 
-#     <bouton>Connecter</bouton>
-# </form>
+
